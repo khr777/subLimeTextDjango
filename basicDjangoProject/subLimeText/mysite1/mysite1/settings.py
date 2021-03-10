7ea0cfcb046a4cbd454f3dec5f9e2c695cbb18dd
khr777 <kim5638yw@gmail.com>
2021-03-10 22:30:16 +0900

    sublime text editor로 Django 연습 리포지터리 새로 생성

--- basicDjangoProject/subLimeText/mysite1/mysite1/settings.py
+++ basicDjangoProject/subLimeText/mysite1/mysite1/settings.py
@@ -29,14 +29,17 @@ ALLOWED_HOSTS = []
 
 
 # Application definition
-
+# 사이트를 좀 더 수월하게 만들 수 있도록 제공해주는 어플리케이션 
+# 우리가 만든 앱들을 여기에 입력해주어야 사용할 수 있다.
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
+    'django.contrib.admin', # 관리용 사이트 
+    'django.contrib.auth', # 인증 시스템 
+    'django.contrib.contenttypes', # 컨텐츠 타입을 위한 프레임워크
+    'django.contrib.sessions', # 세션 프레임워크
+    'django.contrib.messages', # 메세징 관련 프레임워크
+    'django.contrib.staticfiles', # 정적 파일을 관리하기 위한 프레임워크 
+    # PollsConfig 클래스의 경로 표현을 유의해서 추가한다.
+    'polls.apps.PollsConfig'
 ]
 
 MIDDLEWARE = [
@@ -75,9 +78,7 @@ WSGI_APPLICATION = 'mysite1.wsgi.application'
 
 DATABASES = {
     'default': {
-        #'ENGINE': 'django.db.backends.sqlite3', 'django.db.backends.postgresql', 
-        #'django.db.backends.mysql', 'django.db.backends.oracle'
-        'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
--- basicDjangoProject/subLimeText/mysite1/polls/models.py
+++ basicDjangoProject/subLimeText/mysite1/polls/models.py
@@ -1,13 +1,16 @@
 from django.db import models
 
+# 여기에 정의된 모델(데이터베이스 테이블)은 migrate 명령을 이용하여
+# 실제 데이터베이스에 적용할 수 있다.
+
 # 2개의 모델 객체 생성(테이블)
 class Question(models.Model):
-	question_txt = models.CharField(max_Length=200)
+	question_txt = models.CharField(max_length=200)
 	pub_date = models.DateTimeField('date published')
 
 class Choice(models.Model):
 	question = models.ForeignKey(Question, on_delete=models.CASCADE)
-	choice_text = models.CharField(max_Length=200)
+	choice_text = models.CharField(max_length=200)
 	votes = models.IntegerField(default=0)
 
 
