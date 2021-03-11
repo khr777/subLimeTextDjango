3afd66738c413d9d8c83a20d998cc0af99b89a45
khr777 <kim5638yw@gmail.com>
2021-03-11 15:17:18 +0900

    datetime를 import 해보고 관계형 데이터 추가, 수정, 삭제해보기
    datetime를 import 해보고 관계형 데이터 추가, 수정, 삭제해보기.

--- basicDjangoProject/subLimeText/mysite1/polls/models.py
+++ basicDjangoProject/subLimeText/mysite1/polls/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+import datetime
 # 여기에 정의된 모델(데이터베이스 테이블)은 migrate 명령을 이용하여
 # 실제 데이터베이스에 적용할 수 있다.
 
@@ -13,7 +14,7 @@ class Question(models.Model):
 
 	# 가장 최근 데이터가 있는지 없는지를 판단하기 위한 메서드
 	def published_recently(self):
-		return self.timezone.now() - datetime.timedelta(days = 1)
+		return self.pub_date >= timezone.now() - datetime.timedelta(days = 1)
 		# 하루를 뺀 값인 24시간 안에 있는 데이터가 있느냐 없느냐 확인하기 위한 방법.
 		# 최신 데이터인지를 판단하는.
 
