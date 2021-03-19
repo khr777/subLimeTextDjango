72cfb63b273acd0a2236beddb43279b06d66d79c
khr777 <kim5638yw@gmail.com>
2021-03-19 13:27:37 +0900

    views 파일을 사용하지 않고 화면을 출력하는 방법
    views 파일을 사용하지 않고 화면을 출력하는 방법.

--- basicDjangoProject/subLimeText/exprj1/helloprj/hello/urls.py
+++ basicDjangoProject/subLimeText/exprj1/helloprj/hello/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
 from . import views
 
+from django.views.generic import TemplateView
+
 # from . 의 .은 현재 폴더를 의미.
 # views 모듈을 불러와야 하므로 import 
 # views 모듈의 함수 index를 사용하는 것 
 
 # 앱의 urlpatterns에서는 include() 함수를 사용하지 않는다.
 urlpatterns = [
+	path('temp-extends/', TemplateView.as_view(template_name = "child.html")),
 	path('', views.index)
 ]
