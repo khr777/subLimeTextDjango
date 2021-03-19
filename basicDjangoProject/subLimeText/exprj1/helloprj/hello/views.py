84d3ce4c29f68c7704f14dba29252ca9d7760f17
khr777 <kim5638yw@gmail.com>
2021-03-19 10:56:09 +0900

    template 상속 연습
    template 상속 연습.

--- basicDjangoProject/subLimeText/exprj1/helloprj/hello/views.py
+++ basicDjangoProject/subLimeText/exprj1/helloprj/hello/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from datetime import datetime
+
 # index 함수를 활용
 def index(request):
-	return HttpResponse("hello django!!")
+	# return HttpResponse("hello django!!")
+	today = datetime.now().date()
+	daysOfWeek = ['월요일', '화요일', '수요일', '목요일', '금요일', '토요일', '일요일']
+	context = {"today" : today, "daysOfWeek" : daysOfWeek}
+	return render(request, 'hi.html', context)
+
+
